e8c00e9c27f0e2ef09a3ec1870ce13978adb7b42
Kaushik8511 <kaushikpra1999@gmail.com>
2023-04-07 23:08:51 +0530

    working with json in java

---
+++ Json-in-java/src/main/java/JsonAndJavaObject.java
@@ -0,0 +1,25 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonAndJavaObject {
+	
+	private static ObjectMapper mObjectMapper = new ObjectMapper();
+	
+	private static void toJson(User user) throws JsonProcessingException {
+		String json = mObjectMapper.writeValueAsString(user);
+		System.out.println(json);
+	}
+	
+	private static void fromJson(String json) throws JsonMappingException, JsonProcessingException {
+		User user = mObjectMapper.readValue(json, User.class);
+		System.out.println(user);
+	}
+	
+	public static void main(String[] args) throws JsonProcessingException {
+		User user = new User("Kaushik", "Prajapati", 23);
+		toJson(user);
+		fromJson("{\"firstName\":\"xyz\",\"lastName\":\"pqr\",\"age\":404}");
+	}
+	
+}
