5f4bda0c5081ee00b6c123a57879262b1a3bdffb
Kaushik8511 <kaushikpra1999@gmail.com>
2023-04-15 00:50:23 +0530

    file uploading in spring boot to the file system

---
+++ file-uploading/src/main/java/com/example/controller/FileUpload.java
@@ -0,0 +1,61 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.helper.FileUploadHelper;
+
+@RestController
+public class FileUpload {
+
+	@Autowired
+	private FileUploadHelper fileUploadHelper;
+	
+	
+	@GetMapping("/")
+	public String hello() {
+		return "Hello guys";
+	}
+	
+	@PostMapping("/upload")
+	public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+		
+		try {
+			System.out.println("Origin name: " + file.getOriginalFilename());
+			System.out.println("file name: " + file.getName());
+			System.out.println("content type: " + file.getContentType());
+			//returns 0 if empty
+			System.out.println("size: "+ file.getSize());
+			System.out.println("class: " + file.getClass());
+		
+		
+			
+			//validations
+			if(file.isEmpty()) {
+				return new ResponseEntity<String>("Please upload the file", HttpStatusCode.valueOf(404));
+			}
+			if(!file.getContentType().equals("image/jpeg")) {
+				return new ResponseEntity<String>("Please upload image", HttpStatus.NOT_FOUND);
+			}
+			
+			
+			//file upload code
+			if(fileUploadHelper.uploadFile(file)) {
+				return ResponseEntity.ok("File uploaded successfully");
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return new ResponseEntity<String>("bad request", HttpStatusCode.valueOf(400));
+	}
+	
+}
---
+++ file-uploading/src/main/java/com/example/helper/FileUploadHelper.java
@@ -0,0 +1,51 @@
+package com.example.helper;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import javax.print.attribute.standard.DateTimeAtCompleted;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class FileUploadHelper {
+	
+	public final String UPLOAD_DIRECTORY = "D:\\Spring 2.0\\file-uploading\\src\\main\\resources\\static\\image";
+	
+	public boolean uploadFile(MultipartFile myFile) {
+		boolean uploaded = false;
+		
+		try {
+//			//reading the file
+//			InputStream iStream = myFile.getInputStream();
+//			byte data[] = new byte[iStream.available()];
+//			iStream.read(data);
+//			iStream.close();
+//			
+//			//write the file
+//			FileOutputStream foStream = new FileOutputStream(UPLOAD_DIRECTORY + File.separator + myFile.getOriginalFilename());
+//			System.out.println(UPLOAD_DIRECTORY + File.separator + myFile.getOriginalFilename());
+//			foStream.write(data);
+//			foStream.flush();
+//			foStream.close();
+			
+			//Alternative
+			String path = UPLOAD_DIRECTORY + File.separator + myFile.getOriginalFilename();
+			Files.copy(myFile.getInputStream(), Paths.get(path), StandardCopyOption.REPLACE_EXISTING);
+			
+			uploaded = true;
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return uploaded;
+	}
+	
+}
